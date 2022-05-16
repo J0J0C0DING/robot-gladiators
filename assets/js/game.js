0a8210a3b5139c782c253f4446487a55150f0c8b
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-05-16 18:21:00 -0500

    fixed player name value

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,9 +3,18 @@ let randomNumber = function (min, max) {
 
   return value;
 };
+let getPlayerName = function () {
+  let name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  return name;
+};
 
 let playerInfo = {
-  name: prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -101,7 +110,6 @@ var startGame = function () {
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
       var pickedEnemyObj = enemyInfo[i];
 
       pickedEnemyObj.health = randomNumber(40, 60);
